b34128349cf38d8da7f86020fe0d0b7d43bfeb57
Uyen Nguyen (Susan) <99068423+SusanUyen@users.noreply.github.com>
2024-07-24 20:05:23 +1000

    Update exercise1337.py

--- set8/exercise1337.py
+++ set8/exercise1337.py
@@ -147,7 +147,19 @@ def set_it_on_fire(input_string="very naughty boy") -> str:
     interleave_string = fire_emoji + fire_emoji.join(input_string) + fire_emoji
     return interleave_string
 
+def the_chain_gang_5(the_value) -> bool:
+    """Take the_value, subtract 5 from it, and return True if the value we end up with it 5.
 
+    You don't get anything for free this far into the quiz, you can't
+    use the == operator or the - operator, and you must use two of the
+    functions you've already written.
+
+    TIP: you've already written a function that returns True if the value is 5
+    TIP: you've already written a function that subtracts 5
+    """
+
+    new_value = take_five(the_value)
+    return is_it_5(new_value)
 
 def pet_filter(letter="a") -> List:
     """Return a list of pets whose name contains the character 'letter'"""
@@ -301,6 +313,8 @@ if __name__ == "__main__":
     print("n_counter:", n_counter(7))
     print("fizz_buzz:", fizz_buzz())
     print("put_behind_bars:", set_it_on_fire())
+    print("chaing gang 5", the_chain_gang_5(5))
+    print("chaing gang 5", the_chain_gang_5(10))
     print("pet_filter:", pet_filter())
     print("best_letter_for_pets:", best_letter_for_pets())
     print("make_filler_text_dictionary:", make_filler_text_dictionary())
